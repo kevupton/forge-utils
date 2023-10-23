dca5ea21221c4854258225baf0c7efd3c7d0c383
Kevin Upton <k_upton@live.com>
2023-10-23 22:19:51 +1100

    replaced with network name

--- src/scripts/generate-network-json/index.ts
+++ src/scripts/generate-network-json/index.ts
@@ -1,7 +1,6 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import * as glob from 'glob';
-import * as net from 'net';
+import fs from 'fs';
+import path from 'path';
+import glob from 'glob';
 
 interface Transaction {
   contractName?: string;
@@ -48,6 +47,7 @@ export function generateNetworkJson({output, dir}: Options) {
     const pieces = file.split('/');
     const networkId = pieces[pieces.length - 2];
     network[networkId] = network[networkId] || {};
+    const networkName = subgraphData[networkId];
     contractTimestamps[networkId] = contractTimestamps[networkId] || {};
 
     const receipt = (hash: string): Receipt | undefined =>
@@ -70,7 +70,7 @@ export function generateNetworkJson({output, dir}: Options) {
             currentBlockNumber,
           ];
 
-          network[networkId][tx.contractName] = {
+          network[networkName][tx.contractName] = {
             address: tx.contractAddress,
             startBlock: currentBlockNumber,
           };
@@ -84,3 +84,41 @@ export function generateNetworkJson({output, dir}: Options) {
     JSON.stringify(network, null, 2)
   );
 }
+
+const subgraphData: Record<string, string> = {
+  1: 'mainnet',
+  5: 'goerli',
+  10: 'optimism',
+  56: 'bsc',
+  77: 'poa-sokol',
+  97: 'chapel',
+  99: 'poa-core',
+  100: 'gnosis',
+  122: 'fuse',
+  137: 'matic',
+  250: 'fantom',
+  280: 'zkstark-testnet',
+  288: 'bobai',
+  420: 'goerli',
+  1023: 'clover',
+  1284: 'moonbeam',
+  1285: 'moonriver',
+  1287: 'mbase',
+  4002: 'fantom-testnet',
+  42161: 'arbitrum-one',
+  421613: 'arbitrum-goerli',
+  42220: 'celo',
+  43113: 'fuji',
+  43114: 'avalanche',
+  44787: 'celo-alfajores',
+  80001: 'mumbai',
+  1313161554: 'aurora',
+  1313161555: 'aurora-testnet',
+  1666600000: 'harmony',
+  864531: 'base',
+  543451: 'scroll-sapolio',
+  1101: 'polygon-zkevm',
+  324: 'zksync-era',
+  1115011: 'sapolio',
+  1442: 'polygon-zkevm-testnet',
+};
